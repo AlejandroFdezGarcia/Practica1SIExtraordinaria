6a44957da1f6c570887b31423e3540fa697d9d44
AlejandroFdezGarcia <al.fernandezg.2020@alumnos.urjc.es>
2023-06-06 12:11:21 +0200

    Segunda modificación Ejercicio4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -38,3 +38,58 @@ plt.title('Número de alertas en el tiempo (por día)')
 plt.show()
 
 conn.close()
+
+######################## Dispositios más vulnerables:
+conn = sqlite3.connect('Practica1.db')
+cursor = conn.cursor()
+
+query = """SELECT origin, COUNT(*) AS total FROM alertas GROUP BY origin ORDER BY total DESC"""
+
+cursor.execute(query)
+results = cursor.fetchall()
+
+conn.close()
+
+num_devices = 10
+x_labels = [result[0] for result in results[:num_devices]]
+y_values = [result[1] for result in results[:num_devices]]
+
+plt.bar(x_labels, y_values)
+plt.title(f"Top {num_devices} dispositivos más vulnerables")
+plt.xlabel("Dispositivo")
+plt.ylabel("Número de alertas")
+plt.show()
+
+
+
+##################################### Media de puertos abiertos:
+conn = sqlite3.connect('Practica1.db')
+
+query = '''SELECT clasification, AVG(port) as avg_port, COUNT(*) as count FROM alertas GROUP BY clasification'''
+
+cursor = conn.cursor()
+cursor.execute(query)
+results = cursor.fetchall()
+
+clasifications = []
+avg_ports = []
+total_services = []
+insecure_services = []
+
+for row in results:
+    clasification, avg_port, count = row
+    clasifications.append(clasification)
+    avg_ports.append(avg_port)
+    total_services.append(count)
+    insecure_services.append(0)
+
+fig, ax = plt.subplots()
+ax.bar(clasifications, avg_ports, label='Media de puertos abiertos')
+ax.plot(clasifications, total_services, label='Total de servicios detectados', color='red')
+ax.plot(clasifications, insecure_services, label='Servicios inseguros', color='orange')
+ax.legend()
+ax.set_xlabel('Clasificación')
+ax.set_ylabel('Puertos')
+ax.set_title('Media de puertos abiertos por clasificación de vulnerabilidad')
+plt.xticks(rotation=90)
+plt.show()
